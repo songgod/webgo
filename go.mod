bd69fa5be7ff78f6765c28ee55bd41314dacc23f
songgod <songgod@163.com>
2023-02-16 19:23:24 +0800

    second commit

--- go.mod
+++ go.mod
@@ -28,10 +28,10 @@ require (
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.4.4
+	gorm.io/gorm v1.24.0
 )
 
 require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
-	gorm.io/gorm v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"net/http"
+	"back/common"
+	"back/router"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "gorm.io/driver/sqlite"
 )
 
-type User struct {
-	gorm.Model
-	Name      string `gorm:"type:varchar(20);not null"`
-	Telephone string `gorm:"type:varchar(110);not null;unique"`
-	Password  string `gorm:"type:varchar(255);not null"`
-}
-
 func main() {
 
-	db, err := initDB()
+	db, err := common.InitDB()
 
 	if err != nil {
 		panic(err)
@@ -27,56 +20,7 @@ func main() {
 
 	e := gin.Default()
 
-	e.POST("/api/auth/regist", func(ctx *gin.Context) {
-		name := ctx.PostForm("name")
-		telephone := ctx.PostForm("telephone")
-		password := ctx.PostForm("password")
-
-		if len(name) == 0 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 442, "msg": "名字不能为空."})
-			return
-		}
-		if len(telephone) != 11 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 442, "msg": "手机号码应该为11位."})
-			return
-		}
-		if len(password) < 6 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 442, "msg": "密码不能小于6位."})
-			return
-		}
-
-		if isTelephoneExist(db, telephone) {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 442, "msg": "手机号已经注册."})
-			return
-		}
-
-		newUser := User{
-			Name:      name,
-			Telephone: telephone,
-			Password:  password,
-		}
-
-		db.Create(&newUser)
-
-		ctx.JSON(http.StatusOK, gin.H{"code": 200, "msg": "注册成功"})
-	})
+	router.CollectRouter(e)
 
 	e.Run()
-}
-
-func initDB() (db *gorm.DB, err error) {
-	db, err = gorm.Open("sqlite3", "user.db")
-	if err != nil {
-		return nil, err
-	}
-
-	db.AutoMigrate(&User{})
-
-	return
-}
-
-func isTelephoneExist(db *gorm.DB, tel string) bool {
-	var user User
-	db.Where("telephone=?", tel).First(&user)
-	return user.ID != 0
 }
